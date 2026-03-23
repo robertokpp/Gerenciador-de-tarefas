b014172eabf4cf4e2a23911a6f0ca29c21cffec7
Roberto Kennedy <roberto.kpp@gmail.com>
2026-03-23 14:50:33 -0500

    feat: refatorar método de criação de tarefas para incluir validação de dados e persistência no banco

--- src/controllers/tasks-controller.ts
+++ src/controllers/tasks-controller.ts
@@ -1,14 +1,25 @@
 import { Request, Response } from "express";
 import {z} from "zod"
+import { Priority } from "../../generated/prisma/enums";
+import { prisma } from "@/database/prisma";
 
 class TasksController {
   async create(request: Request, response: Response) {
     const bodySchema = z.object({
-    
+      title: z.string(),
+      description: z.string(),
+      priority: z.string(),
     })
 
+    const { title, description , priority } = bodySchema.parse(request.body)
 
-
+    const task = await prisma.tasks.create({
+      data: {
+        title: title,
+        description: description,
+        priority: priority,
+      }
+    })
     return response.json("ok");
   }
 }
