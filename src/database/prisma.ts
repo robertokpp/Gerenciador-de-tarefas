8f91622263f129d75ab1f649d2a9503c4fc0d848
Roberto Kennedy <roberto.kpp@gmail.com>
2026-03-23 20:37:27 -0500

    feat: adicionar dotenv como dependência e refatorar conexão com o Prisma

--- src/database/prisma.ts
+++ src/database/prisma.ts
@@ -1,10 +1,12 @@
 
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 import { PrismaClient } from "../../generated/prisma/client";
 
 
 const connectionString = `${process.env.DATABASE_URL}`;
-const adapter = new PrismaPg({ connectionString });
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
 export { prisma };
