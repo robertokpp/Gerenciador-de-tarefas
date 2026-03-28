4013704637650ba015b94c36b4cb19e22b111f95
Roberto Kennedy <roberto.kpp@gmail.com>
2026-03-27 22:44:52 -0500

    feat: adicionar verificação de membro da equipe ao atualizar tarefa atribuída

--- src/controllers/tasksAssigned-controller.ts
+++ src/controllers/tasksAssigned-controller.ts
@@ -1,13 +1,50 @@
 import { prisma } from "@/database/prisma";
+import { AppError } from "@/utils/AppError";
 import { Request, Response } from "express";
 import { z } from "zod";
 
-class tasksAssignedController{
-  async update(request: Request, response:Response){
+class TasksAssignedController {
+  async update(request: Request, response: Response) {
+    const paramsSchema = z.object({
+      id: z.coerce.number().int().positive(),
+    });
+
     const bodySchema = z.object({
       userId: z.number().int().positive()
+    });
+
+    const { id } = paramsSchema.parse(request.params)
+    const { userId } = bodySchema.parse(request.body);
+
+    const task = await prisma.tasks.findFirst({
+      where:{
+        id
+      }
     })
 
-    const userCheck = await prisma.teamMembers
+    
+    
+    //Verifica se o usuário pertence ao time
+    const exists = await prisma.teamMembers.findFirst({
+      where: {
+        userId,
+        teamId: task?.teamId
+      },
+    });
+
+    if (!exists) {
+      throw new AppError("This user is not part of this team.");
+    }
+
+    const taskUpdate = await prisma.tasks.update({
+      where: { id },
+      data:{
+        assignedTo: userId
+      }
+    })
+
+    return response.status(200).json(taskUpdate);
   }
 }
+
+export { TasksAssignedController };
--- src/routes/tasks-router.ts
+++ src/routes/tasks-router.ts
@@ -3,11 +3,15 @@ import { TasksController } from "@/controllers/tasks-controller";
 import { TasksStatusController } from "@/controllers/tasksStatus-controller";
 import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
 import { TasksPriorityController } from "@/controllers/tasksPriority-controller";
+import { TasksAssignedController  } from "@/controllers/tasksAssigned-controller";
+import { teamsRouter } from "./teams-router";
+
 
 const tasksRouter = Router();
 const tasksController = new TasksController();
 const tasksStatusController = new TasksStatusController();
 const tasksPriorityController = new TasksPriorityController();
+const tasksAssignedController  = new TasksAssignedController()
 
 tasksRouter.use(ensureAuthenticated);
 
@@ -20,5 +24,7 @@ tasksRouter.get("/status", tasksStatusController.index);
 tasksRouter.patch("/:id/status", tasksStatusController.update);
 
 tasksRouter.get("/priority", tasksPriorityController.index);
+
+tasksRouter.patch("/:id/assign", tasksAssignedController.update)
 
 export { tasksRouter };
