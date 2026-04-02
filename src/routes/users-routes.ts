91e795e33c406265dc1fea5d76183872894df8b4
Roberto Kennedy <roberto.kpp@gmail.com>
2026-04-01 22:43:00 -0500

    feat: adicionar verificação de autenticação e autorização na rota de listagem de usuários

--- src/routes/users-routes.ts
+++ src/routes/users-routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { UserController } from "@/controllers/users-controller";
+import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization";
+import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
 
 const userRoutes = Router();
 const userController = new UserController();
 
 userRoutes.post("/", userController.create);
-userRoutes.get("/", userController.index)
+userRoutes.get("/",ensureAuthenticated, verifyUserAuthorization(["admin"]), userController.index)
 
 export { userRoutes };
